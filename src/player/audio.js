cf7641259ba6f02da000319ed861fd5ef1710540
unknown <you@example.com>
2024-07-27 19:20:16 +0300

    fist comm

---
+++ src/player/audio.js
@@ -0,0 +1,26 @@
+import song1 from './music/song1.mp3';
+import song2 from './music/song2.mp3';
+import song3 from './music/song3.mp3';
+import img1 from './images/img1.jpg';
+import img2 from './images/img2.jpg';
+import img3 from './images/img3.jpg';
+
+
+export const songsdata = [
+    {
+        title: "Stolen Loops - Проснись",
+        url: song3,
+        img: img1
+    },
+    {
+        title: "Summer of Haze - Coldest Night",
+        url: song1,
+        img: img3
+    },
+    {
+        title: "ENCASSATOR - Lil Sky!",
+        url: song2,
+        img: img2
+    }
+]
+
